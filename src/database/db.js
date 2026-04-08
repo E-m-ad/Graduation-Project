d8499d964841aefbed3ecd0e828d44f6c01d3858
E-m-ad <fci.emad@gmail.com>
2026-04-08 10:30:47 +0200

    fix prisma client runtime import

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import { PrismaClient } from "../generated/prisma/client.ts";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 dotenv.config();
